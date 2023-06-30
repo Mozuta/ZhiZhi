8332228bc1fee5a0dcca9a73183bdfc91356001a
Qiwei Shan <113519356+Mozuta@users.noreply.github.com>
2023-06-30 15:49:37 +0800

    1.3.3难度选择更新

---
+++ app/src/main/java/com/hex/zz/Exe_choose_Activity.java
@@ -0,0 +1,14 @@
+package com.hex.zz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Exe_choose_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exe_choose);
+    }
+}
